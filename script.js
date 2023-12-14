bca62c08751c432be80ffea8c99e889d6e947cbb
MiguelMeixoeiro <149668283+MiguelMeixoeiro@users.noreply.github.com>
2023-12-14 12:36:29 +0100

    mejoras wuachis

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// script.js
 const itemsPerPage = 5;
 const productContainer = document.getElementById('product-container');
 const prevPageBtn = document.getElementById('prevPageBtn');
@@ -7,168 +6,195 @@ const currentPageSpan = document.getElementById('currentPage');
 const cartCount = document.getElementById('cart');
 const modal = document.getElementById('modal');
 const closeModal = document.getElementsByClassName('close')[0];
+const loadingGif = document.getElementById('loading-gif');
+const additionalGif1 = document.getElementsByClassName('additional-gif1')[0];
+const additionalGif2 = document.getElementsByClassName('additional-gif2')[0];
+const wishlistButton = document.createElement('button');
+wishlistButton.textContent = '♥ Wishlist';
+wishlistButton.classList.add('wishlist-button');
+
+document.querySelector('header').appendChild(wishlistButton);
 
 let cartItems = 0;
 let cartProducts = [];
-const loadingGif = document.getElementById('loading-gif'); // Agregado para el GIF de carga
-const additionalGif1 = document.getElementsByClassName('additional-gif1')[0];
-const additionalGif2 = document.getElementsByClassName('additional-gif2')[0];
+let wishlist = [];
 
-// Mostrar el GIF de carga antes de hacer la solicitud a la API
 loadingGif.style.display = 'block';
 additionalGif1.style.display = 'block';
 additionalGif2.style.display = 'block';
 
 fetch('http://makeup-api.herokuapp.com/api/v1/products.json')
-  .then(response => response.json())
-  .then(data => {
-    // Ocultar el GIF de carga después de que se completa la solicitud de la API
-    loadingGif.style.display = 'none';
-    additionalGif1.style.display = 'none';
-    additionalGif2.style.display = 'none';
+    .then(response => response.json())
+    .then(data => {
+        loadingGif.style.display = 'none';
+        additionalGif1.style.display = 'none';
+        additionalGif2.style.display = 'none';
 
-    console.log('Data from API:', data);
+        console.log('Data from API:', data);
 
-    let currentPage = 1;
+        let currentPage = 1;
 
-    function displayProducts(page) {
-      console.log('Displaying products for page:', page);
+        function displayProducts(page) {
+            const startIndex = (page - 1) * itemsPerPage;
+            const endIndex = startIndex + itemsPerPage;
+            const productsToShow = data.slice(startIndex, endIndex);
 
-      const startIndex = (page - 1) * itemsPerPage;
-      const endIndex = startIndex + itemsPerPage;
+            productContainer.innerHTML = '';
 
-      const productsToShow = data.slice(startIndex, endIndex);
+            productsToShow.forEach(product => {
+                const productDiv = document.createElement('div');
+                productDiv.classList.add('product');
 
-      productContainer.innerHTML = '';
+                const productImage = document.createElement('img');
 
-      productsToShow.forEach(product => {
-        const productDiv = document.createElement('div');
-        productDiv.classList.add('product');
+                if (product.image_link) {
+                    productImage.src = product.image_link;
 
-        const productImage = document.createElement('img');
+                    productImage.onerror = function() {
+                        if (this.naturalWidth === 0 && this.naturalHeight === 0) {
+                            this.src = './nofoto.jpg';
+                        }
+                    };
+                } else {
+                    productImage.src = './nofoto.jpg';
+                }
 
-        if (product.image_link) {
-          productImage.src = product.image_link;
+                productImage.alt = product.name;
 
-          productImage.onerror = function() {
-            if (this.naturalWidth === 0 && this.naturalHeight === 0) {
-              this.src = './nofoto.jpg';
-            }
-          };
-        } else {
-          productImage.src = './nofoto.jpg';
+                const productInfoDiv = document.createElement('div');
+                productInfoDiv.classList.add('product-info');
+
+                const productName = document.createElement('h2');
+                productName.textContent = product.name;
+
+                const productBrand = document.createElement('p');
+                productBrand.textContent = `Marca: ${product.brand}`;
+
+                const productPrice = document.createElement('p');
+                if (typeof product.price !== 'undefined') {
+                    productPrice.textContent = `Precio: $${product.price}`;
+                } else {
+                    productPrice.textContent = 'Precio no disponible';
+                }
+
+                productInfoDiv.appendChild(productName);
+                productInfoDiv.appendChild(productBrand);
+                productInfoDiv.appendChild(productPrice);
+
+                productDiv.appendChild(productImage);
+                productDiv.appendChild(productInfoDiv);
+
+                const buttonsContainer = document.createElement('div');
+                buttonsContainer.classList.add('buttons-container');
+
+                const heartButton = document.createElement('button');
+                heartButton.textContent = '♥';
+                heartButton.classList.add('heart-button');
+
+                // Verificar si el producto está en la wishlist
+                if (wishlist.some(item => item.id === product.id)) {
+                    heartButton.classList.add('filled');
+                }
+
+                buttonsContainer.appendChild(heartButton);
+
+                const plusButton = document.createElement('button');
+                plusButton.textContent = '+ info';
+                plusButton.classList.add('plus-button');
+                buttonsContainer.appendChild(plusButton);
+
+                const cartButton = document.createElement('button');
+                cartButton.textContent = '🛒';
+                cartButton.classList.add('cart-button');
+                buttonsContainer.appendChild(cartButton);
+
+                productDiv.appendChild(buttonsContainer);
+                productContainer.appendChild(productDiv);
+
+                heartButton.addEventListener('click', () => toggleHeart(heartButton, product));
+                plusButton.addEventListener('click', openModal);
+                cartButton.addEventListener('click', () => {
+                    addToCart(product);
+                });
+            });
+
+            const totalPages = Math.ceil(data.length / itemsPerPage);
+            currentPageSpan.textContent = page;
+
+            prevPageBtn.disabled = page === 1;
+            nextPageBtn.disabled = page === totalPages;
         }
 
-        productImage.alt = product.name;
-
-        const productInfoDiv = document.createElement('div');
-        productInfoDiv.classList.add('product-info');
-
-        const productName = document.createElement('h2');
-        productName.textContent = product.name;
-
-        const productBrand = document.createElement('p');
-        productBrand.textContent = `Marca: ${product.brand}`;
-
-        const productPrice = document.createElement('p');
-        if (typeof product.price !== 'undefined') {
-          productPrice.textContent = `Precio: $${product.price}`;
-        } else {
-          productPrice.textContent = 'Precio no disponible';
+        function updatePage(direction) {
+            currentPage += direction;
+            displayProducts(currentPage);
         }
 
-        productInfoDiv.appendChild(productName);
-        productInfoDiv.appendChild(productBrand);
-        productInfoDiv.appendChild(productPrice);
+        prevPageBtn.addEventListener('click', () => updatePage(-1));
+        nextPageBtn.addEventListener('click', () => updatePage(1));
 
-        productDiv.appendChild(productImage);
-        productDiv.appendChild(productInfoDiv);
+        displayProducts(currentPage);
+    })
+    .catch(error => {
+        loadingGif.style.display = 'none';
+        console.error('Error al obtener datos de la API:', error);
+    });
 
-        const buttonsContainer = document.createElement('div');
-        buttonsContainer.classList.add('buttons-container'); // Nuevo div para los botones
+function toggleHeart(heartButton, product) {
+    heartButton.classList.toggle('filled');
 
-        const heartButton = document.createElement('button');
-        heartButton.textContent = '♥';
-        heartButton.classList.add('heart-button');
-        buttonsContainer.appendChild(heartButton);
+    const productIndex = wishlist.findIndex(item => item.id === product.id);
 
-        const plusButton = document.createElement('button');
-        plusButton.textContent = '+ info';
-        plusButton.classList.add('plus-button');
-        buttonsContainer.appendChild(plusButton);
-
-        const cartButton = document.createElement('button');
-        cartButton.textContent = '🛒';
-        cartButton.classList.add('cart-button');
-        buttonsContainer.appendChild(cartButton);
-
-        productDiv.appendChild(buttonsContainer); // Agregamos el nuevo div de botones al producto
-
-        productContainer.appendChild(productDiv);
-
-        // Event listeners para los botones
-        heartButton.addEventListener('click', () => toggleHeart(heartButton));
-        plusButton.addEventListener('click', openModal);
-        cartButton.addEventListener('click', () => {
-          addToCart(product);
-        });
-      });
-
-      const totalPages = Math.ceil(data.length / itemsPerPage);
-      currentPageSpan.textContent = page;
-
-      prevPageBtn.disabled = page === 1;
-      nextPageBtn.disabled = page === totalPages;
+    if (heartButton.classList.contains('filled')) {
+        if (productIndex === -1) {
+            wishlist.push(product);
+        }
+    } else {
+        if (productIndex !== -1) {
+            wishlist.splice(productIndex, 1);
+        }
     }
-
-    function updatePage(direction) {
-      currentPage += direction;
-      displayProducts(currentPage);
-    }
-
-    prevPageBtn.addEventListener('click', () => updatePage(-1));
-    nextPageBtn.addEventListener('click', () => updatePage(1));
-
-    displayProducts(currentPage);
-  })
-  .catch(error => {
-    // Ocultar el GIF de carga en caso de error
-    loadingGif.style.display = 'none';
-    console.error('Error al obtener datos de la API:', error);
-  });
-
-// ... (resto del código)
-
-// Función para cambiar el estado del corazón
-function toggleHeart(heartButton) {
-  heartButton.classList.toggle('filled');
 }
 
-// Función para abrir el modal
 function openModal() {
-  modal.style.display = 'block';
+    modal.style.display = 'block';
 }
 
-// Event listener para cerrar el modal
 closeModal.onclick = function() {
-  modal.style.display = 'none';
+    modal.style.display = 'none';
 };
 
-// Función para agregar al carrito
 function addToCart(product) {
-  cartItems++;
-  cartProducts.push(product);
-
-  cartCount.textContent = `🛒: ${cartItems}`;
+    cartItems++;
+    cartProducts.push(product);
+    cartCount.textContent = `🛒: ${cartItems}`;
 }
 
-// Event listener para el botón de carrito en el footer
 cartCount.addEventListener('click', () => {
-  openCartPage();
+    openCartPage();
 });
 
-// Función para abrir la página del carrito
 function openCartPage() {
-  localStorage.setItem('cartProducts', JSON.stringify(cartProducts));
-  window.location.href = 'carrito.html';
-};
+    localStorage.setItem('cartProducts', JSON.stringify(cartProducts));
+    window.location.href = 'carrito.html';
+}
+
+wishlistButton.addEventListener('click', () => {
+    openWishlistModal();
+});
+
+function openWishlistModal() {
+    const wishlistContent = wishlist.length > 0
+        ? wishlist.map(product => `<p>${product.name}</p>`).join('')
+        : 'Tu wishlist está vacía';
+
+    modal.innerHTML = `
+        <div class="modal-content">
+            <span class="close">&times;</span>
+            <h2>Wishlist</h2>
+            ${wishlistContent}
+        </div>
+    `;
+
+    modal.style.display = 'block';
+}
